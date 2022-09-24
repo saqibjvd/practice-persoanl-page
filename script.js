842ee2b67693752a05b0d9ca2c569fce86b74478
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2022-09-24 11:56:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,37 @@ test("There are at least 500 words on the page", () => {
 
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);
+
+var sheep, other;
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+sheep = 0;
+other = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_sheep_count = document.getElementById('sheep_count');
+  if (getNumberOrString(document.getElementById('text').value) == 'sheep') {
+    let element_sheep_count2 = document.getElementById('sheep_count');
+    element_sheep_count2.innerText = sheep;
+    sheep = (typeof sheep === 'number' ? sheep : 0) + 1;
+  } else {
+    let element_other_count = document.getElementById('other_count');
+    element_other_count.innerText = other;
+    other = (typeof other === 'number' ? other : 0) + 1;
+  }
+
+});
+if (false) {
+}
+
